a10ac47fa9351e444e3a1431fefbaea398a777a2
SantiF14 <101821966+SantiF14@users.noreply.github.com>
2022-07-04 13:26:08 -0300

    Se cambio la BD

--- VacunAsist/gestion_de_usuarios/models.py
+++ VacunAsist/gestion_de_usuarios/models.py
@@ -62,7 +62,9 @@ class Usuario(AbstractBaseUser):
     password = models.CharField(null=True, max_length=50, verbose_name='contrasenia')  #ver max_length por hashing/encriptacion
     clave_alfanumerica = models.CharField(max_length=5)
     vacunatorio_pref = models.ForeignKey("Vacunatorio", on_delete=models.SET_NULL, null=True) #deberiamos cambiar las HU en tal caso, noguta
-    
+    opciones_rol = [("Vac","Vacunador"),("Adm","Administrador"),("User","Usuario comun")]
+    rol_actual = models.CharField(max_length=4, choices=opciones_rol, default=None ,blank=True, null=True)
+
     class Meta:
         verbose_name_plural = "Usuarios"
         app_label = 'gestion_de_usuarios'
